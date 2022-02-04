b64b1e1978aaf71066c899e47242fbbd9c9ade05
BergBarbosa <bergbarboza@protonmail.com>
2022-02-04 17:28:23 -0300

    edit Get(int id)

--- SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -24,7 +24,7 @@ namespace SuperHeroAPI.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<List<SuperHero>>> Get(int id)
         {
-            var hero = heroes.Find(h => h.Id == id);
+            var hero = await _context.SuperHeroes.FindAsync(id);
             if (hero == null) return BadRequest("Hero not found");
             return Ok(hero);
         }
