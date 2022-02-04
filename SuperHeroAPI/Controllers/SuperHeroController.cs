24f92de357eeb49690574ae6e6c8077d393c6524
BergBarbosa <bergbarboza@protonmail.com>
2022-02-04 17:36:41 -0300

    edit UpdateHero

--- SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -32,22 +32,27 @@ namespace SuperHeroAPI.Controllers
         [HttpPost]
         public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
         {
-            heroes.Add(hero);
-            return Ok(heroes);
+            _context.SuperHeroes.Add(hero);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
         }
 
         [HttpPut]
         public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero request)
         {
-            var hero = heroes.Find(h => h.Id == request.Id);
-            if (hero == null) return BadRequest("Hero not found");
+            var dbHero = await _context.SuperHeroes.FindAsync(request.Id);
+            if (dbHero == null) return BadRequest("Hero not found");
 
-            hero.Name = request.Name;
-            hero.FirstName = request.FirstName;
-            hero.LastName = request.LastName;
-            hero.Place = request.Place;
+
+            dbHero.Name = request.Name;
+            dbHero.FirstName = request.FirstName;
+            dbHero.LastName = request.LastName;
+            dbHero.Place = request.Place;
+
+            await _context.SaveChangesAsync();
            
-            return Ok(heroes);
+            return Ok(await _context.SuperHeroes.ToListAsync());
         }
 
         [HttpDelete("{id}")]
